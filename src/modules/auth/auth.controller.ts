60c29a6c39e6afa574304f5b8ea11d2046a6a64b
nikul kakadiya <kakadiyamikul@gmail.com>
2024-02-12 15:02:19 +0530

    added some code

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,28 +1,38 @@
-import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
 
-
 @Controller('auth')
 export class AuthController {
-    constructor(private _authService: AuthService){
+  constructor(private _authService: AuthService) {}
 
-    }
+  @Get()
+  @UseGuards(AuthGuard('jwt'))
+  async get(@Request() req) {
+    // console.log('req--',req.user);
+    return this._authService.getHello();
+  }
 
-    @Post('login')
-    async login(@Body() body){       
-        return this._authService.login(body);
-    }
+  @Post('login')
+  async login(@Body() body) {
+    return this._authService.login(body);
+  }
 
-    @Post('register')
-    async register(@Body() body){       
-        return this._authService.register(body);
-    }
-    
-    @Get()
-    @UseGuards(AuthGuard('jwt'))
-    async get(@Request() req){    
-        // console.log('req--',req.user);             
-        return this._authService.getHello();
-    }
+  @Post('register')
+  async register(@Body() body) {
+    return this._authService.register(body);
+  }
+
+  @Get('getUserByToken')
+  @UseGuards(AuthGuard('jwt'))
+  async getUserByToken(@Request() req) {
+    return this._authService.getUserByToken(req.user);
+  }
 }
--- src/modules/notes/notes.service.ts
+++ src/modules/notes/notes.service.ts
@@ -1,99 +1,113 @@
 import { Injectable } from '@nestjs/common';
-import { Notes } from '../../entities/dbdemo'
+import { Notes, User } from '../../entities/dbdemo';
 import { v1 as uuidv1 } from 'uuid';
 
 @Injectable()
 export class NotesService {
-    constructor(){
-        
-    }
-    
-    async getAllNotes(){
-        const data = await Notes.findAll();
-        return {
-            status: 'success',
-            data,
-            message: 'Successfully',
-        };
-    }
+  constructor() {}
 
-    async addNote(payload: any) {
-        try{            
-            const note = await Notes.create({
-                noteId: uuidv1(),
-                title: payload.title,
-                description: payload.description,
-                tag: payload.tag,
-                userId: payload.userId,
-            });
-            return {
-                status: 'success',
-                note,
-                message: 'User Created Successfully',
-            };
-        }catch(error){
-            console.log(error);
-            return { status: 'error', message: 'An error occurred while adding note' };         
-        }
-    }
+  async getAllNotes(userId: any) {
+    // const data = await Notes.findAll({
+    //   where: { userId }, // Filter notes by userId
+    //   include: [{ model: User, attributes: ['userId', 'name', 'email'] }], // Include user information
+    // });
+    const data = await Notes.findAll({
+      where: { userId },
+      attributes: ['noteId', 'title', 'description', 'tag'],
+    });
+    return {
+      status: 'success',
+      data,
+      message: 'Successfully',
+    };
+  }
 
-    async updateNote(noteId: string, payload: any) {
-        try {
-            const findNote = await Notes.findOne({
-                where: { noteId } // Ensure the note belongs to the requesting user
-            });
-    
-            if (!findNote) {
-                return { status: 'error', message: 'Note not found' };
-            }
-
-            const note = await Notes.update(
-                {
-                    // Update the fields with the new values from the payload
-                    title: payload.title,
-                    description: payload.description,
-                    tag: payload.tag,
-                    userId: payload.userId
-                },
-                {
-                    where: { noteId: noteId } // Find the note by its ID
-                }
-            );
-    
-            if (!note) {
-                return { status: 'error', message: 'Note not found' };
-            }
-    
-            return {
-                status: 'success',
-                note,
-                message: 'Note updated successfully',
-            };
-        } catch (error) {
-            console.log(error);
-            return { status: 'error', message: 'An error occurred while updating note' };
-        }
+  async addNote(payload: any) {
+    try {
+      const note = await Notes.create({
+        noteId: uuidv1(),
+        title: payload.title,
+        description: payload.description,
+        tag: payload.tag,
+        userId: payload.userId,
+      });
+      return {
+        status: 'success',
+        note,
+        message: 'User Created Successfully',
+      };
+    } catch (error) {
+      console.log(error);
+      return {
+        status: 'error',
+        message: 'An error occurred while adding note',
+      };
     }
+  }
 
-    async deleteNote(noteId: string) {
-        try {
-            const note = await Notes.findOne({
-                where: { noteId} // Find the note by its ID
-            });
-    
-            if (!note) {
-                return { status: 'error', message: 'Note not found' };
-            }
-    
-             // Delete the note from the database
-            await Notes.destroy({where: { noteId}})
-            return {
-                status: 'success',
-                message: 'Note deleted successfully',
-            };
-        } catch (error) {
-            console.log(error);
-            return { status: 'error', message: 'An error occurred while deleting note' };
-        }
+  async updateNote(noteId: string, payload: any) {
+    try {
+      const findNote = await Notes.findOne({
+        where: { noteId }, // Ensure the note belongs to the requesting user
+      });
+
+      if (!findNote) {
+        return { status: 'error', message: 'Note not found' };
+      }
+
+      const note = await Notes.update(
+        {
+          // Update the fields with the new values from the payload
+          title: payload.title,
+          description: payload.description,
+          tag: payload.tag,
+          userId: payload.userId,
+        },
+        {
+          where: { noteId: noteId }, // Find the note by its ID
+        },
+      );
+
+      if (!note) {
+        return { status: 'error', message: 'Note not found' };
+      }
+
+      return {
+        status: 'success',
+        note,
+        message: 'Note updated successfully',
+      };
+    } catch (error) {
+      console.log(error);
+      return {
+        status: 'error',
+        message: 'An error occurred while updating note',
+      };
     }
+  }
+
+  async deleteNote(noteId: string) {
+    try {
+      const note = await Notes.findOne({
+        where: { noteId }, // Find the note by its ID
+      });
+
+      if (!note) {
+        return { status: 'error', message: 'Note not found' };
+      }
+
+      // Delete the note from the database
+      await Notes.destroy({ where: { noteId } });
+      return {
+        status: 'success',
+        message: 'Note deleted successfully',
+      };
+    } catch (error) {
+      console.log(error);
+      return {
+        status: 'error',
+        message: 'An error occurred while deleting note',
+      };
+    }
+  }
 }
